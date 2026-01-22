b1d74844a755d9450daccd8cb0d5a474c7d8d2ba
crummycrushed <aiden.jsr@gmail.com>
2026-01-22 21:50:41 +0530

    Input function

---
+++ session4/input.py
@@ -0,0 +1,22 @@
+# input() -- allows a program/code to receive information from the user
+
+
+#name = input("Enter your name: ")
+#print(f"Hello, {name}")
+
+
+#age = input("Enter your age: ")
+#intage = int(age)   #int(str)
+#print(type(intage))
+#print(f"Age of {name} is {age}")
+
+
+#text = "hello"
+#inttext = int(text)
+#print(inttext)
+
+text = 5
+text1 = '15'
+inttext2 = int(text1)  #int('15')
+print(inttext2)
+# str =  f"Age of {name} is {age}" {} -> name  --> rahul, {} --> age 24
