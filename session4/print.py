1e4696883784a3ac83f4197be638c41574eec50a
crummycrushed <aiden.jsr@gmail.com>
2026-01-22 21:33:56 +0530

    Understanding print function

---
+++ session4/print.py
@@ -0,0 +1,15 @@
+# print often needs to communicate results to users. print()
+
+
+print("hello world")
+
+a = 5
+c = "hello"
+print(a)
+print ("The value of a is: ",  a, 6, 11)  # string + int
+print(a+5.0+4+6)
+
+
+print(f"The value of a is {a}. And add {c}")
+
+# {} {} {}
