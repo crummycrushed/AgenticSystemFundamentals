4db212fc1f37e1f22d8a53cbe3e3a1ab38a451ee
crummycrushed <aiden.jsr@gmail.com>
2026-01-27 20:56:31 +0530

    Boolean first class concept learning

---
+++ session5/boolean_check.py
@@ -0,0 +1,33 @@
+is_logged_in = True
+
+# if user is logged in , print ("hello user")
+
+if is_logged_in:  # 60 >= 40 : True or False # if True:
+    print("hello user")
+
+
+# >=, <=, >, <, ==
+
+if is_logged_in == True:  ## True == True, True
+    print("hello user")
+
+
+
+
+atm_pin = 1234
+entered_pin = 1111
+
+# pin accepted or pin wrong
+
+if entered_pin == atm_pin:
+    print("pin accepted")
+else:
+    print("ping wrong")
+
+
+
+
+is_rainy_day = True
+
+if is_rainy_day == True:   #True or False True == "True"
+    print("Yes")
