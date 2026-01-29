ca1c9fccdd2e8c8519f9831aefbe7cbedaf435de
crummycrushed <aiden.jsr@gmail.com>
2026-01-29 21:29:20 +0530

    While loop examples

---
+++ session6/while_loop.py
@@ -0,0 +1,24 @@
+count = 0
+
+
+while count < 3:  # while the condition is true keep looping , count = 0 0 < 3, count = 5 < 3
+    #print(count) #0
+    #print("hello") # hello
+    count = count + 3 # count = 0 + 5 = 5
+
+
+
+
+# u ask user to enter password till he she doesn't enter the correct passwor
+actual_password = "admin123"
+password = ""
+
+while password != actual_password: #"admin123" != "admin123"
+       password = input("Enter your password: ") #password = "aferd" 
+
+
+
+
+
+
+
