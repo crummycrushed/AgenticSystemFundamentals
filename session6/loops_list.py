6cc4ce7ddd6b8f21acb27b5c6d79ceaa088e2e90
crummycrushed <aiden.jsr@gmail.com>
2026-01-29 21:01:54 +0530

    Loops with list

---
+++ session6/loops_list.py
@@ -0,0 +1,18 @@
+names = ["A", "B", "C"]
+
+# range(5) = [0, 1, 2, 3, 4]
+
+                        
+for nx in names: # ["A", "B", "C"]
+    print(nx)
+
+
+
+a = []
+
+
+
+# python say
+# take one name
+# print it
+# move to next
