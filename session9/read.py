35f7ef509dd332244c89851c6943df8511f7f53f
crummycrushed <aiden.jsr@gmail.com>
2026-02-05 21:00:04 +0530

    Learning about reading a file

---
+++ session9/read.py
@@ -0,0 +1,48 @@
+
+#variable = open -- connection --> file, read mode
+file = open("/Users/priyanshu/Documents/AgenticSystemFundamentals/session9/hello.txt", "r")
+#print(file.read()) #read 
+file.close()
+
+
+# read everything , we basically use file.read()
+
+
+#file = open("/Users/priyanshu/Documents/AgenticSystemFundamentals/session9/hello.txt", "r")
+#print(file.read()) #read 
+# index our error 
+# error happens  ???
+
+#file.close() # before closing the files
+
+
+
+# with statement 
+
+# file = open()
+with open("/Users/priyanshu/Documents/AgenticSystemFundamentals/session9/hello.txt", "r") as i:
+    content = i.read()
+    #print(content)
+
+
+
+#python gurantees: file close, even if crash happens, even if exception occues
+
+
+# read line by line
+with open("/Users/priyanshu/Documents/AgenticSystemFundamentals/session9/hello.txt", "r") as file:
+    for line in file:
+        content = line.strip()
+        if content == "Shubhendu":
+            continue
+        #print(content)
+
+
+## readlines() --> readlines stores all lines into a list
+
+with open("/Users/priyanshu/Documents/AgenticSystemFundamentals/session9/hello.txt", "r") as file:
+    lines = file.readlines()
+    for i in lines:
+        print(i.strip())
+
+    
