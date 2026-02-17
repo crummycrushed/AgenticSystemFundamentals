ef8787da80a82841d3171e5dac7ddedc396a2d63
crummycrushed <aiden.jsr@gmail.com>
2026-02-17 20:43:49 +0530

    Basic numpy working

---
+++ session10/vectorization.py
@@ -0,0 +1,23 @@
+numbers = [1, 2, 3, 4]
+# out = [2, 4, 6, 8]
+
+squared = []
+for i in numbers:
+    squared.append(i * 2)
+
+# print(squared)
+
+import numpy as np
+
+numbers_np = np.array([1, 2, 3, 4])
+squared_np = numbers_np * 2
+print(squared_np)
+
+
+
+# multiply [10, 20, 30] by 5 
+arr = np.array([10, 20, 30])
+multiply = arr * 5
+print(multiply)
+
+
