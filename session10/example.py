18ba58bdba09d271d22e29d4e32dbad742db358c
crummycrushed <aiden.jsr@gmail.com>
2026-02-17 20:32:42 +0530

    Example of numpy

---
+++ session10/example.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr)
+
+# imported the library
+# np.array() : creates a numpy array 
+# printed array
+# All elements (1,2,3,4) are stored in continous memory 
