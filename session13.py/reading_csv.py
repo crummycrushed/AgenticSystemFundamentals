5aa10dd63fbc87da52c68c685257a81f573a14fb
crummycrushed <aiden.jsr@gmail.com>
2026-02-24 21:45:42 +0530

    fundamental inspection methods

--- session13.py/reading_csv.py
+++ session13.py/reading_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("employee.csv")
-print(df)
+#print(df)
 
 
 # That single line:
@@ -9,3 +9,39 @@ print(df)
 # parese rows and columns
 # data type infer
 # constrcuted a dataframe
+
+
+val = df.head(10)
+#print(val)
+
+
+#this helps to verify:
+# column names
+# data format
+# basic structure 
+# no of columns
+
+
+val_2 = df.tail(10)
+#print(val_2)
+
+
+val_3 = df.iloc[2 : 7]
+#print(val_3)
+
+
+
+
+info = df.info()
+#print(df.info())
+
+
+#provides:
+# number of rows
+# columns names
+# data types
+# count of non null values
+
+
+desc = df.describe()
+print(desc)
