3086c52a95d707ecbd8506270023dea657e98ca5
crummycrushed <aiden.jsr@gmail.com>
2026-02-24 21:00:03 +0530

    Dataframes and loading files from csv

---
+++ session13.py/dataframes.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Math": [90, 80,85],
+    "Science": [88, 92, 89]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
+# created index automatically
+# Understand data type automatically 
+# Formatted as table
+
+
---
+++ session13.py/reading_csv.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv("employee.csv")
+print(df)
+
+
+# That single line:
+# opens the file
+# parese rows and columns
+# data type infer
+# constrcuted a dataframe
