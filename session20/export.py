f035d82488d8d8439bd57e466f344b24ec7f805a
crummycrushed <aiden.jsr@gmail.com>
2026-03-12 22:00:57 +0530

    Kladieo demo

---
+++ session20/export.py
@@ -0,0 +1,30 @@
+import plotly.express as px #simple interface for creating charts
+import pandas as pd
+import numpy as np
+
+df_cluster = pd.DataFrame({
+    "x": np.random.randn(100), #generated 100 random numbers
+    "y": np.random.randn(100),
+    "group": np.random.choice(["Group1", "Group2", "Group3"], 100) # randomly assiedn each point to group A , group b or grpou c
+})
+## Create a dataset with 100 rows , randomly assign each row with a group either A or B
+
+#print(df_cluster)
+
+fig = px.scatter(
+    df_cluster,
+    x="x",
+    y="y",
+    color="group", #color each point based on the group value
+    title="Cluster visualization",
+    color_discrete_map={
+        "Group2": "blue",
+        "Group1": "green",
+        "Group3": "red"
+    }
+)
+
+fig.write_image("plot3.png", width=1000, height=600)
+
+
+
