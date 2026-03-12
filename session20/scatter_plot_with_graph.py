1cd63873834ed4547879e653759e6be3df9286fb
crummycrushed <aiden.jsr@gmail.com>
2026-03-12 21:17:48 +0530

    using discrete map

--- session20/scatter_plot_with_graph.py
+++ session20/scatter_plot_with_graph.py
@@ -5,7 +5,7 @@ import numpy as np
 df_cluster = pd.DataFrame({
     "x": np.random.randn(100), #generated 100 random numbers
     "y": np.random.randn(100),
-    "group": np.random.choice(["A", "B", "C"], 100) # randomly assiedn each point to group A , group b or grpou c
+    "group": np.random.choice(["Group1", "Group2", "Group3"], 100) # randomly assiedn each point to group A , group b or grpou c
 })
 ## Create a dataset with 100 rows , randomly assign each row with a group either A or B
 
@@ -17,7 +17,11 @@ fig = px.scatter(
     y="y",
     color="group", #color each point based on the group value
     title="Cluster visualization",
-    color_discrete_sequence=["#1f77b4", "#ff7f0e"] #
+    color_discrete_map={
+        "Group2": "blue",
+        "Group1": "green",
+        "Group3": "red"
+    }
 )
 
 fig.show()
